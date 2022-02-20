02e827cc2037ff48a45cd360a7b727f837e690a8
Ganesh <krganesh1611@gmail.com>
2022-02-20 13:19:11 +0530

    scrapped to greater depth

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const axios = require("axios").default;
 const cheerio = require("cheerio");
+const { find } = require("domutils");
 const fs = require("fs");
-
+const url = "https://www.moneycontrol.com";
 axios.get("https://www.moneycontrol.com/markets/indian-indices/").then(function(html){
-    fs.writeFileSync("test.html", html.data, "utf-8");
+    // fs.writeFileSync("test.html", html.data, "utf-8");
     handlehtml(html.data);
 }).catch(function(error){
     console.log("Error Encountered!!!");
@@ -15,4 +16,5 @@ function handlehtml(html){
     let content = $(".clearfix.inditablecnt");
     let ele = $(content).find(".ntlist");
     fs.writeFileSync("test2.html", $(ele).html(), "utf-8");
+    let hrefValue = $(ele).find("li[data-name='NIFTY BANK'] > h2 > a").attr('href');
 }
